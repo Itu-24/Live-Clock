f93f36d52cc12a5a87ef6ec91aaf6b165bbac29d
Akash Singh <122321576+Itu-24@users.noreply.github.com>
2024-02-19 01:52:33 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,21 +1,38 @@
-function put() {
-    document.getElementById('pp').innerHTML = "Hello Akash";
-}
-function time() {
-    let mytime = new Date();
-
-    document.getElementById('hrs').innerHTML = mytime.getHours() + '  Hr  ';
-    document.getElementById('min').innerHTML = mytime.getMinutes() + '  Min ';
-    document.getElementById('sec').innerHTML = mytime.getSeconds() + '  Sec';
-}
 
 let box = document.getElementById('box');
 box.style.display='none';
 
+function time() {
+    let mytime = new Date();
+
+    document.getElementById('long').innerHTML = mytime.getHours() + '  Hr  ';
+    document.getElementById('medium').innerHTML = mytime.getMinutes() + '  Min ';
+    document.getElementById('small').innerHTML = mytime.getSeconds() + '  Sec';
+    
+}
+
+function date(){
+
+    let mydate=new Date();
+    document.getElementById('long').innerHTML=mydate.getFullYear();
+    document.getElementById('medium').innerHTML=mydate.getMonth();
+    document.getElementById('small').innerHTML=mydate.getDate();
+    
+}
+
 function showtime() {
     
     box.style.display='flex';
-    btn.style.display='none';
+    container.style.display='none';
+
     setInterval(time, 1000);
 
 }
+
+function showdate(){
+    box.style.display='flex';
+    container.style.display='none';
+
+    setInterval(date, 1000);
+}
+
